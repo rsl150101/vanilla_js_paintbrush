eb51f7392c3c1d242641a1835a45c00881295139
rsl150101 <dnwnd501@naver.com>
2022-01-09 16:40:21 +0900

    Update app.js
    Added canvas defalut background color, save function
    
    Lock right-click menu

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const colors = document.getElementsByClassName("controls__color");
 const brushSize = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const clearBtn = document.getElementById("jsClear");
+const saveBtn = document.getElementById("jsSave");
 
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.strokeStyle = "black";
 ctx.fillStyle = "black";
 ctx.lineWidth = 2.5;
@@ -65,6 +68,7 @@ function handleModeClick() {
 
 function handleCanvasClick() {
   if (filling) {
+    //캔버스 x = 0, y = 0 좌표 기준으로 canvas.width 넓이 만큼, canvas.height 높이 만큼 채워준다
     ctx.fillRect(0, 0, canvas.width, canvas.height);
   }
 }
@@ -72,6 +76,21 @@ function handleCanvasClick() {
 function canvasClear() {
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function canvasSave() {
+  const link = document.createElement("a");
+
+  // toDataURL 은 지정된 type parameter(기본값 PNG)에 의해 지정된 포맷의 포맷의 이미지 표현을 포함한 data URL을 반환
+  link.href = canvas.toDataURL("image/png");
+  // download 는 a 의 attribute 로 url로 가는 대신 다운로드함
+  link.download = "PaintJS";
+  link.click();
+}
+
+function handleCM(event) {
+  // preventDefault는 기본 동작을 실행하지 않도록 해서 우클릭 시 나오는 메뉴 이벤트가 나오지 않게 한다.
+  event.preventDefault();
 }
 
 if (canvas) {
@@ -82,6 +101,8 @@ if (canvas) {
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseenter", onMouseEnter);
   canvas.addEventListener("click", handleCanvasClick);
+  // contextmenu 는 우클릭 했을 때 나오는 메뉴 이벤트
+  canvas.addEventListener("contextmenu", handleCM);
 }
 
 // Array.from 은 오브젝트를 배열로 만들어준다
@@ -91,3 +112,4 @@ Array.from(colors).forEach((color) =>
 
 mode.addEventListener("click", handleModeClick);
 clearBtn.addEventListener("click", canvasClear);
+saveBtn.addEventListener("click", canvasSave);
